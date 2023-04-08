5cb0376fca2d37437d8501e4e22c3c291c8bc9c8
AitorSabin <aitorsabinprol@gmail.com>
2023-04-08 11:42:51 +0200

    removed metadata from repo

--- Udemy/POO/src/ejercicio8/Main.java
+++ Udemy/POO/src/ejercicio8/Main.java
@@ -1,146 +1,139 @@
 package ejercicio8;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	public static Scanner sc= new Scanner(System.in);
 	
-	public static int buscarPaquete(ArrayList <Paquete> paqueteArrayList, int numeroPaquete, int contadorPaquete) {
-		int indice =0;
-		boolean encontrado = false;
-		
-		for(int i=0; i<paqueteArrayList.size();i++) {
-			if(paqueteArrayList.get(i).getNumeroPaquete() == numeroPaquete) {
-				encontrado = true;
-				indice = i;
+	public static Sucursal crearSucursal() {
+		System.out.println("\nIntroduce el numero de Sucursal: ");
+		int numeroSucursal  = sc.nextInt();
+		System.out.println("\nIntroduce la direccion de la Surcursal: ");
+		sc.nextLine();
+		String direccion = sc.nextLine();
+		System.out.println("\nIntroduce la ciudad de la Sucursal: ");
+		String ciudad = sc.nextLine();
+		return new Sucursal(numeroSucursal, direccion, ciudad);
+	}
+	
+	public static void enviarPaquete(List<Sucursal> listaSucursales) {
+		System.out.println("\nIntroduce el numero de Sucursal: ");
+		int numeroSucursal  = sc.nextInt();
+		Sucursal sucursal = buscarSucursal(listaSucursales, numeroSucursal);
+		if(sucursal == null) {
+			System.out.println("La sucursal no existe");
+		}else {
+			sucursal.addPaquete(crearPaquete());
+		}
+	}
+	
+	public static Paquete crearPaquete() {
+		System.out.println("\nIntroduce el numero de Paquete: ");
+		int numeroPaquete = sc.nextInt();
+		System.out.println("\nIntroduce el dni del Remitente: ");
+		String dni= sc.next();
+		System.out.println("\nIntroduce el peso del Paquete: ");
+		double peso= sc.nextDouble();
+		System.out.println("\nIntroduce la prioridad (0=Normal, 1=Alta, 2=Express): ");
+		int prioridad= sc.nextInt();
+		return new Paquete(numeroPaquete, dni, peso, prioridad);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void mostrarSucursal(List<Sucursal> listaSucursales) {
+		System.out.println("Introduce el numero de Sucursal: ");
+		int numeroSucursal = sc.nextInt();
+		Sucursal sucursal = buscarSucursal(listaSucursales, numeroSucursal);
+		System.out.println(sucursal== null ? "La Sucursal no existe" : sucursal.mostrarDatosSucursal());
+	}
+	
+	public static Sucursal buscarSucursal(List<Sucursal> listaSucursales, int numeroSucursal) {	
+		for (Sucursal sucursal: listaSucursales) {
+			if(numeroSucursal == sucursal.getNumeroSucursal()) {
+				return sucursal;
 			}
 		}
-		if(encontrado == false) {
-			indice = -1;
-		}
-		return indice;
+		return null;				
 	}
-
-	public static int buscarSucursal(ArrayList <Sucursal> sucursalesArrayList, int numeroSucursal, int contadorSucursal) {
-		int indice =0;
-		boolean encontrado = false;
+	
+	public static void buscarPaquete(List<Sucursal> listaSucursales, int numeroPaquete) {
 		
-		for(int i=0; i<sucursalesArrayList.size();i++) {
-			if(sucursalesArrayList.get(i).getNumeroSucursal() == numeroSucursal) {
-				encontrado = true;
-				indice = i;
-			}
-		}
-		if(encontrado == false) {
-			indice = -1;
-		}
-		return indice;
 	}
+	
+	
+//	public static String buscarSucursal(List<Sucursal> listaSucursales ) {
+//		System.out.println("Introduce el numero de Sucursal: ");
+//		int numeroSucursal = sc.nextInt();
+//		for (Sucursal sucursal: listaSucursales) {
+//			if(numeroSucursal == sucursal.getNumeroSucursal()) {
+//				return sucursal.mostrarDatosSucursal();
+//			}
+//		}
+//		return "La Sucursal no existe";				
+//	}
 	
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		int numeroSucursal;
-		int numeroPaquete;
-		int prioridad;
-		int opcion;
-		int contadorSucursal=0;
-		int contadorPaquete=0;
-		int indiceSucursal;
-		int indicePaquete;
-		double peso;
-		double precioPaquete;
-		String dni;
-		String direccion;
-		String ciudad;
-		//Sucursal sucursales[] = new Sucursal[50];
-		//Paquete paquetes[] = new Paquete[100];
-		ArrayList <Sucursal> sucursalesArrayList = new ArrayList<Sucursal>();
-		ArrayList <Paquete> paqueteArrayList = new ArrayList<Paquete>();
 		
+		int opcion;
+		List<Sucursal> listaSucursales = new ArrayList<Sucursal>();
+
 		do {
 			System.out.println("\t.:MENU:.");
 			System.out.println("1. Crear una nueva Sucursal");
 			System.out.println("2. Enviar Paquete");
 			System.out.println("3. Consultar Sucursal");
 			System.out.println("4. Consultar Paquete");
-			System.out.println("5. Mostrat todas las sucursales");
+			System.out.println("5. Mostrar todas las sucursales");
 			System.out.println("6. Mostrar todos los paquetes");
 			System.out.println("7. Salir");
 			System.out.println("Introduce la opcion del menu; ");
 			opcion = sc.nextInt();
 			
 			switch(opcion) {
-				case 1: System.out.println("\nIntroduce el numero de Sucursal: ");
-						numeroSucursal = sc.nextInt();
-						System.out.println("\nIntroduce la direccion de la Surcursal: ");
-						sc.nextLine();
-						direccion = sc.nextLine();
-						System.out.println("\nIntroduce la ciudad de la Sucursal: ");
-						ciudad = sc.next();
-						
-						sucursalesArrayList.add(new Sucursal(numeroSucursal, direccion, ciudad));
-						//sucursales[contadorSucursal] = new Sucursal(numeroSucursal, direccion, ciudad);
-						contadorSucursal++;
+				case 1: listaSucursales.add(crearSucursal());
 						break;
-				case 2: System.out.println("\nIntroduce el numero de la Sucursal: ");
-						numeroSucursal = sc.nextInt();
-						indiceSucursal = buscarSucursal(sucursalesArrayList, numeroSucursal, contadorSucursal);
-						
-						if(indiceSucursal == -1) {
-							System.out.println("La sucursal no existe");
-						}else {
-							System.out.println("\nIntroduce el numero de Paquete: ");
-							numeroPaquete = sc.nextInt();
-							System.out.println("\nIntroduce el dni del Remitente: ");
-							dni= sc.next();
-							System.out.println("\nIntroduce el peso del Paquete: ");
-							peso= sc.nextDouble();
-							System.out.println("\nIntroduce la prioridad (0=Normal, 1=Alta, 2=Express): ");
-							prioridad= sc.nextInt();
-							
-							paqueteArrayList.add(new Paquete(numeroPaquete, dni, peso, prioridad));
-							
-							precioPaquete = sucursalesArrayList.get(indiceSucursal).calcularPrecio(paqueteArrayList.get(contadorPaquete));
-							System.out.println("\nEl precio es: "+precioPaquete+"€");
-							contadorPaquete++;
-						}
+				case 2: enviarPaquete(listaSucursales);
 						break;
-				case 3: System.out.println("Introduce el numero de Sucursal: ");
-						numeroSucursal = sc.nextInt();
-						
-						indiceSucursal = buscarSucursal(sucursalesArrayList, numeroSucursal, contadorSucursal);
-						
-						if(indiceSucursal == -1) {
-							System.out.println("\nLa sucursal no existe");
-						}else {
-							System.out.println("\nLos datos de la sucursal son: ");
-							System.out.println(sucursalesArrayList.get(indiceSucursal).mostrarDatosSucursal());
-						}
+				case 3: mostrarSucursal(listaSucursales);
 						break;
 				case 4: System.out.println("Introduce el numero de Paquete: ");
-						numeroPaquete = sc.nextInt();
 						
-						indicePaquete = buscarPaquete(paqueteArrayList, numeroPaquete, contadorPaquete);
 						
-						if(indicePaquete == -1) {
-							System.out.println("\\nEl paquete no existe");
-						}else {
-							System.out.println("\nLos datos del paquete son: ");
-							System.out.println(paqueteArrayList.get(indicePaquete).mostrarDatosPaquete());
-						}
 						break;
+						
 				case 5: 
-						for(Sucursal sucursal: sucursalesArrayList) {
-							System.out.println("\nLos datos de la sucursal son los siguientes: ");
-							System.out.println(sucursal.mostrarDatosSucursal());
-						}
+						
 						break;
 				case 6: 
-						for(Paquete paquetes: paqueteArrayList) {
-							System.out.println("\nLos datos del paquete son los siguientes: ");
-							System.out.println(paquetes.mostrarDatosPaquete());
-						}
-						break;
+						
 				case 7: break;
 				default: System.out.println("Error, opcion de menu incorrecta");
 						break;
--- Udemy/POO/src/ejercicio8/Sucursal.java
+++ Udemy/POO/src/ejercicio8/Sucursal.java
@@ -1,15 +1,19 @@
 package ejercicio8;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sucursal {
 	private int numeroSucursal;
 	private String direccion;
 	private String ciudad;
-	Paquete paquetes;
+	private List<Paquete> listaPaquetes;
 	
 	public Sucursal(int numeroSucursal, String direccion, String ciudad) {
 		this.numeroSucursal = numeroSucursal;
 		this.direccion = direccion;
 		this.ciudad = ciudad;
+		this.listaPaquetes = new ArrayList<Paquete>();
 	}
 
 	public int getNumeroSucursal() {
@@ -23,23 +27,32 @@ public class Sucursal {
 	public String getCiudad() {
 		return ciudad;
 	}
+
+	public void addPaquete(Paquete paquete) {
+		listaPaquetes.add(paquete);
+	}
 	
+	public List<Paquete> getListaPaquetes() {
+		return listaPaquetes;
+	}
+
 	public String mostrarDatosSucursal() {
 		return "\nNumero de Sucursal: "+numeroSucursal+
 				" \nDireccion de la Sucursal: "+direccion+
 				" \nCiudad de la Sucursal: "+ciudad;
 	}
 	
-	public double calcularPrecio(Paquete paquetes) {
-		double precio = paquetes.getPeso();
+	public double calcularPrecio2(Paquete paquete) {		
 		
-		if(paquetes.getPrioridad()==1) {
-			precio += 10;
+		double precio = paquete.getPeso();
+		
+		if(paquete.getPrioridad()==1) {
+			return precio += 10;
+		} else if(paquete.getPrioridad()==2) {
+			return precio += 20;
+		} else {
+			return precio;			
 		}
-		if(paquetes.getPrioridad()==2) {
-			precio += 20;
-		}
-		return precio;
 	}
 	
 	
